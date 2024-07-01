b4052e42b4686b5c83cad08cfbb5eb92f1a810e1
Beatrice Araújo <beatrice.araujo@sgnsistemas.com.br>
2024-07-01 14:49:49 -0300

    Corpo do Desafio

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style/App.css';
+
+//Login do Usuário
+//Seleção do Personagem
+//Inserção da quantidade de Derrotas e Vitorias  
+
+function App() {
+  return (
+    <>
+    
+
+
+    </>
+  );
+}
+
+export default App;
